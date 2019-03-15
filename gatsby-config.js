0ffdd3d3bd300303bff872d8e1a374b02ffb8c4c
jsphkm <jsphkm12@gmail.com>
2019-03-14 22:02:11 -0700

    Expose gatsby-plugin-catch-links in the configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
