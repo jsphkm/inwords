a7dcb44ad8c6c0e329635c308031f9cedcd290d7
jsphkm <jsphkm12@gmail.com>
2019-03-28 17:11:18 -0700

    Indent blockquote to line up with the viewport

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -38,10 +38,10 @@ bootstrapTheme.overrideThemeStyles = () => {
     blockquote: {
       // margin: '1.3125rem -1.3125rem 2.3125rem -1.3125rem',
       margin: '1.3125rem -1rem 2.3125rem -1rem',
-      padding: '1rem 1.1rem',
+      padding: '1rem 1rem',
       borderLeft: '0.25rem solid #fbc420',
       opacity: '0.9',
-      boxShadow: '0px 0px 10px rgba(0,0,0,0.4)',
+      // boxShadow: '0px 0px 10px rgba(0,0,0,0.4)',
       fontSize: '1rem',
     },
   }
